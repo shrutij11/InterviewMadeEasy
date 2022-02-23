513e95dd8154df0d29d3267ad0306f6bdba0afeb
Shruti <jaiswalshruti1102@gmail.com>
2022-02-23 15:36:40 +0530

    basic styling

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/about', function(req, res, next){
+  res.render('about', { title: 'InterviewMadeEasy - a platform for sharing code.' });
+});
 
 module.exports = router;
