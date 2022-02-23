feb7eadeaea1dc0fa295b7497f34733b68eacfe5
Shruti <jaiswalshruti1102@gmail.com>
2022-02-23 17:59:27 +0530

    contact form

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,13 @@ router.get('/', function(req, res, next) {
 router.get('/about', function(req, res, next){
   res.render('about', { title: 'InterviewMadeEasy - a platform for sharing code.' });
 });
+router.route('/contact')
+.get( function(req, res, next){
+  res.render('contact', { title: 'InterviewMadeEasy - a platform for sharing code.' });
+})
+.post(function(req, res, next){
+  res.render('thank', { title: 'InterviewMadeEasy - a platform for sharing code.' })
+})
 
 module.exports = router;
+ 
